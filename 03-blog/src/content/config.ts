a823fd0e5ec024b131d504068e58376bf4d017ef
Paco <pacors88@gmail.com>
2024-08-11 00:22:14 +0200

    end section 6

--- 03-blog/src/content/config.ts
+++ 03-blog/src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: 'content',
@@ -9,11 +9,29 @@ const blogCollection = defineCollection({
     image: image().refine(img => img.width < 1200, {
       message: 'Image width must be less than 1200px'
     }),
-    author: z.string(),
+
+    // author: z.string(),
+    author: reference('author'),
     tags: z.array(z.string()),
+
+    isDraft: z.boolean().default(false),
+  }),
+});
+
+const authorCollection = defineCollection({
+  type: 'data',
+  schema: ({ image }) => z.object({
+    name: z.string(),
+    avatar: image(),
+    bio: z.string(),
+    twitter: z.string(),
+    linkedIn: z.string(),
+    github: z.string(),
+    subtitle: z.string(),
   }),
 });
 
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 }
